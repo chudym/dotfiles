ca2ea571143b97bc0fc3c01bccb6dd6b61c132de
Marcin Chudy <mchudy@users.noreply.github.com>
2016-01-03 12:49:26 +0100

    Update git submodules in bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+success() { echo "$(tput setaf 2)DONE$(tput sgr0)"; }
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $dir
+olddir=~/dotfiles_old
+files="bashrc vimrc vim emacs"
+
+echo -n 'Updating submodules...'
+git submodule init
+git submodule update --recursive
+success
+
+echo -n "Creating symlinks... "
+mkdir -p $olddir
+cd $dir
+for file in $files; do
+  if [ -f ~/.$file ]
+  then
+    mv ~/.$file $olddir/
+  fi
+    ln -s $dir/$file ~/.$file
+done
+success
+
+echo -n "Installing vim plugins... "
+vim +PluginInstall +qall
+success
